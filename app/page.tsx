c16e807d671c93a2f57670595996a0c1aff4a11e
akshaya-244 <akshayamohan.2401@gmail.com>
2025-11-14 15:16:26 -0800

    Set up TRPC and Inngest

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useTRPC } from "./trpc/client";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export default function Home() {
+  // const trpc = useTRPC()
+  // //useQuery runs the query
+  // const {data} = useQuery(trpc.hello.queryOptions({text: "Atonoa"}))
+
+  const trpc = useTRPC()
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Background job running")
+    }
+  }))
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <Button>Click me </Button>
+    <div className="flex min-h-screen text-black items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+     <Button disabled={invoke.isPending} onClick={() => invoke.mutate({text:"John"})}> Invoke Background job </Button>
     </div>
   );
 }
---
+++ app/trpc/routers/_app.ts
@@ -0,0 +1,33 @@
+import { email, z } from 'zod';
+import { baseProcedure, createTRPCRouter } from '../init';
+import { inngest } from '@/app/inngest/client';
+export const appRouter = createTRPCRouter({
+  invoke: baseProcedure
+  .input(
+    z.object({
+      text: z.string(),
+    })
+  )
+  .mutation(async ({input}) => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: input.text,
+      }
+    })
+    return {ok:"Success"}
+  }),
+  hello: baseProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      }),
+    )
+    .query((opts) => {
+      return {
+        greeting: `hello ${opts.input.text}`,
+      };
+    }),
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
